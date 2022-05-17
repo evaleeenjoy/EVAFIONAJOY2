5ab369b0b5b0bffc01991da428c2001e52a6c33f
evaleeenjoy <105703903+evaleeenjoy@users.noreply.github.com>
2022-05-17 15:40:46 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    led.plotBrightness(4, 2, 255)
+})
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        if (0 == 0) {
+            basic.pause(100)
+        }
+    }
+})
