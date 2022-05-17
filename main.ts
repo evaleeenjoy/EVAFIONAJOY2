70eb691c3c029098cc75945f1af6db2e1220ca9e
evaleeenjoy <105703903+evaleeenjoy@users.noreply.github.com>
2022-05-17 16:23:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,1 @@
-basic.forever(function () {
-    led.plotBrightness(4, 2, 255)
-})
-basic.forever(function () {
-    for (let index = 0; index < 4; index++) {
-        if (0 == 0) {
-            basic.pause(100)
-        }
-    }
-})
+
